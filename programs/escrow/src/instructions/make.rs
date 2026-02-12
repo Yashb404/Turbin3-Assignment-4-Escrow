4ed8c71e01b86b407c1cbf903bae991d48e4befd
Yashb404 <yashbhardwaj7890@gmail.com>
2026-02-13 03:33:35 +0900

    init make struct

--- programs/escrow/src/instructions/make.rs
+++ programs/escrow/src/instructions/make.rs
@@ -1,19 +1,49 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenInterface}; //We can use both older and newer
+use anchor_spl::token_interface::{Mint,TokenAccount, TokenInterface}; //We can use both older and newer
 //use anchor_spl::token::Mint older token program
 //use anchor_spl::Token_2022::Mint newer token program
 
 use crate::state::Escrow;
 
 #[derive(Accounts)]
-pub struct Make<'info>{
+#[instruction(seed: u64)]
+pub struct Make<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
-    #[account(mint::token_program = token_program)]
+    #[account(
+        mint::token_program = token_program
+    )]
     pub mint_a: InterfaceAccount<'info, Mint>,
-    #[account(mint::token_program = token_program)]
+    #[account(
+        mint::token_program = token_program
+    )]
     pub mint_b: InterfaceAccount<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = maker,
+        seeds = [b"escrow", maker.key().as_ref(), seed.to_le_bytes().as_ref()],
+        space = Escrow::DISCRIMINATOR.len() + Escrow::INIT_SPACE,
+        bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
 
-
-    pub token_program: Interface<'info,TokenInterface>
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
 }
+
